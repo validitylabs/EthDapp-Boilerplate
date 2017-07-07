4f7d1e5e79134781cdd9dd07946dad47f9ea12bd
Sebastian C. Bürgel <SCBuergel@users.noreply.github.com>
2017-07-07 20:21:59 +0200

    V1 reading interface

--- custom.js
+++ custom.js
@@ -1,4 +1,6 @@
 $(window).on('load', function() {
+    var contractAddress = "0x57b005EffEd29DFE55EAC92A6833E5328787eD83"; // in Rinkeby testnet!
+    var contractAbi = [{"constant":false,"inputs":[{"name":"s","type":"string"}],"name":"setGreeting","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"getGreeting","outputs":[{"name":"s","type":"string"}],"payable":false,"type":"function"}];
 
     // Checking if Web3 has been injected by the browser (Mist/MetaMask)
     if (typeof web3 !== 'undefined') {
@@ -6,12 +8,18 @@ $(window).on('load', function() {
         $('#content').text('I has web3!!!');
         window.web3 = new Web3(web3.currentProvider);
     } else {
-        $('#content').text('I doesn\'t has web3 :(')
-        console.log('No web3? You should consider trying MetaMask!')
+        $('#content').text('I doesn\'t has web3 :( Please open in Google Chrome Browser and install the Metamask extension.');
+        console.log('No web3? You should consider trying MetaMask!');
+        return;
     }
-
-  // Now you can start your app & access web3 freely:
-
-  // do whatever you want to here!
+    var contractInstance = web3.eth.contract(contractAbi).at(contractAddress);
+    contractInstance.getGreeting(function(error, greeting) {
+        if (error) {
+            $('#content').text('error: ' + error);
+            console.log('error: ' + error);
+            return;
+        }
+        $('#content').text('greeting from contract: ' + greeting);
+    });
 
 });
