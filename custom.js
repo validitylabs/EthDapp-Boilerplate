3ca9244d456cd9d494e7a31dbf3870abc2522b98
Sebastian C. Bürgel <SCBuergel@users.noreply.github.com>
2017-07-07 20:31:04 +0200

    reading form value and writing to contract

--- custom.js
+++ custom.js
@@ -1,4 +1,5 @@
-$(window).on('load', function() {
+$(function() {
+    
     var contractAddress = "0x57b005EffEd29DFE55EAC92A6833E5328787eD83"; // in Rinkeby testnet!
     var contractAbi = [{"constant":false,"inputs":[{"name":"s","type":"string"}],"name":"setGreeting","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"getGreeting","outputs":[{"name":"s","type":"string"}],"payable":false,"type":"function"}];
 
@@ -8,18 +9,36 @@ $(window).on('load', function() {
         $('#content').text('I has web3!!!');
         window.web3 = new Web3(web3.currentProvider);
     } else {
-        $('#content').text('I doesn\'t has web3 :( Please open in Google Chrome Browser and install the Metamask extension.');
-        console.log('No web3? You should consider trying MetaMask!');
+        var errorMsg = 'I doesn\'t has web3 :( Please open in Google Chrome Browser and install the Metamask extension.';
+        $('#content').text(errorMsg);
+        console.log(errorMsg);
         return;
     }
+    
+    // create instance of contract object that we use to interface the smart contract
     var contractInstance = web3.eth.contract(contractAbi).at(contractAddress);
     contractInstance.getGreeting(function(error, greeting) {
         if (error) {
-            $('#content').text('error: ' + error);
-            console.log('error: ' + error);
+            var errorMsg = 'error reading greeting from smart contract: ' + error;
+            $('#content').text(errorMsg);
+            console.log(errorMsg);
             return;
         }
         $('#content').text('greeting from contract: ' + greeting);
+    });
+    
+    $('#my-form').on('submit', function(e) {
+        e.preventDefault(); // cancel the actual submit
+        var newGreeting = $('#greeting').val(); 
+        contractInstance.setGreeting(newGreeting, function(error, txHash) {
+            if (error) {
+                var errorMsg = 'error writing new greeting to smart contract: ' + error;
+                $('#content').text(errorMsg);
+                console.log(errorMsg);
+                return;
+            }
+            $('#content').text('submitted new greeting to blockchain, transaction hash: ' + txHash);
+        });
     });
 
 });
